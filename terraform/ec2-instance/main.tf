8ad43f0c374f71383b4d74b1610304587fd6137a
Francesco Allertsen <francesco@allertsen.xyz>
2021-02-05 12:06:14 +0100

    Add two VPCs and two subnets

--- terraform/ec2-instance/main.tf
+++ terraform/ec2-instance/main.tf
@@ -1,3 +1,21 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
+resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "vpc2" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.vpc2.id
+  cidr_block = "10.0.2.0/24"
+}
